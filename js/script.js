c8d0d3ebaf789ab1995cac687d4f105c0a312ac4
Bunoro <raphsonl@gmail.com>
2020-08-12 23:01:55 +0300

    add function to show and hide fade toggle effect on what we do

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(".design").click(function() {
+    $(".display-design").fadeToggle();
+    $(".hidden-design").fadeToggle();
+});
+
+$(".development").click(function() {
+    $(".display-development").fadeToggle();
+    $(".hidden-development").fadeToggle();
+});
+
+$(".management").click(function() {
+    $(".display-management").fadeToggle();
+    $(".hidden-management").fadeToggle();
+});
