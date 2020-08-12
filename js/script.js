bd29a0857be93990f437fc3ed1ae2ae19a968b20
Bunoro <raphsonl@gmail.com>
2020-08-12 23:21:56 +0300

    add to fix correct link of js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,42 @@
-$(".design").click(function() {
-    $(".display-design").fadeToggle();
-    $(".hidden-design").fadeToggle();
+$(document).ready(function){
+    //to toggle text
+    $(".design").click(function() {
+        $(".display-design").fadeToggle();
+        $(".hidden-design").fadeToggle();
+    });
+    
+    $(".development").click(function() {
+        $(".display-development").fadeToggle();
+        $(".hidden-development").fadeToggle();
+    });
+    
+    $(".management").click(function() {
+        $(".display-management").fadeToggle();
+        $(".hidden-management").fadeToggle();
+    });
+
+    //to toggle text in portfolio class
+    $(".card").hover(function() {
+        $(".toggle-text",this).slideToggle('slow');
+    }, function(){
+        $('.toggle-text',this).fadeToggle('slow');
+    })
+}
+
+//form input validation
+$("#mc-embedded-subscribe-form").on('submit',function(event){
+    event.preventDefault();
+    let name = $("#mce-FNAME").val();
+    let email = $("#mce-EMAIL").val();
+
+    if ($("input#mce-FNAME").val() && $("input#mce-EMAIL").val()){
+        alert ("Hello!!! " + name +".Thank you for reaching out to us,we'll get back to you asap.");
+    }
+    else {
+        alert("Please provide your correct name and email!");
+    }
+
 });
 
-$(".development").click(function() {
-    $(".display-development").fadeToggle();
-    $(".hidden-development").fadeToggle();
-});
 
-$(".management").click(function() {
-    $(".display-management").fadeToggle();
-    $(".hidden-management").fadeToggle();
-});
+
